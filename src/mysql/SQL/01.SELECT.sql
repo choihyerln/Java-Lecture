6bf878278b5b27e2666fd8fdaa825d1956a23470
choihyerln <choi.hyehyehihi@gmail.com>
2022-11-17 09:55:37 +0900

    SELECT구문

--- src/mysql/SQL/01.SELECT.sql
+++ src/mysql/SQL/01.SELECT.sql
@@ -1,8 +1,12 @@
-/*
-* 데이터 조작 언어(DML : Data Manipulation Language)
-*/
+##########################################################
+#
+#   데이터 조작 언어(DML: Data Manipulation Language)
+#
+##########################################################
+
 
 /* 1. Select */
+
 USE world;  # 사용할 데이터베이스 선택
 SHOW TABLES;    # 현 데이터베이스에 있는 테이블 조회
 DESC city;      # city의 테이블 구조 보기
@@ -10,8 +14,24 @@ DESC city;      # city의 테이블 구조 보기
 Select * from city # city 테이블에 있는 내용을 전부 가져오기
 SELECT Name, Population FROM city;  # name, Population 내용만 가져오기
 
+/*
+SELECT 필드명 
+    FROM 테이블명
+    JOIN 테이블명
+    ON 조인 조건
+    WHERE 조건
+    GROUP BY 필드명
+    HAVING 그룹 조건
+    ORDER BY 필드명 순서
+    LIMIT 숫자 OFFSET 숫자;
+*/
 
-/* 조회 조건 Where */
+SELECT * FROM city;     # city 테이블에 있는 내용을 전부 가져오기
+SELECT `Name`, Population FROM city;
+SELECT NOW();
+
+
+/* 1.1 조회 조건 Where */
 
 select * from city where CountryCode='KOR'; # 한국만 조회
 select Name, District, Population from city where CountryCode='KOR';
@@ -45,7 +65,7 @@ select * From City
 
 
 
-/* 순서 */
+/* 1.2 순서 */
 
 # 전세계에서 인구가 800만 이상인 도시를 내림차순으로 조회
 select * From City
@@ -56,7 +76,8 @@ select * From City
 	Where Population>=500000 AND CountryCode = 'KOR'
 	order by District, Population DESC;
 
-/* 갯수 */
+
+/* 1.3 갯수 */
 
 # 전세계 인구수 Top10 (내림차순 -> 리미트 10개설정)
 Select * from city
@@ -84,27 +105,48 @@ Select MAX(Population), MIN(Population) from city
 	Where countrycode = 'KOR';
 
 
-/* 그룹 */
+/* 1.4 함수 */
+
+# 국내 도시의 갯수
+SELECT COUNT(*) FROM city
+	WHERE CountryCode = 'KOR';
+
+# 국내 도시의 평균 인구수
+SELECT ROUND(AVG(Population)) FROM city
+	WHERE CountryCode = 'KOR';
+
+SELECT ROUND(AVG(Population)) AS avgPopulation FROM city	# Aliasing(AS)
+	WHERE CountryCode = 'KOR';
+
+# 국내 도시의 최대/최소 인구수
+SELECT MAX(Population), MIN(Population) FROM city
+	WHERE CountryCode = 'KOR';
+
+
+/* 1.5 그룹 */
 
 # 경기도의 도시 이름
-Select GROUP_CONCAT(Name) from city
-	Where District = 'Kyonggi';
+SELECT GROUP_CONCAT(`Name`) FROM city
+	WHERE District = 'Kyonggi';
 
 # 국내 광역시도 이름
-Select GROUP_CONCAT(District) from city
-	Where Countrycode = 'KOR';
+SELECT GROUP_CONCAT(District) FROM city
+	WHERE CountryCode = 'KOR';
 
-# 국내 도시의 광역시도의 인구수를 내림차순으로 정렬
-Select District, SUM(Population) from city
-	Where countrycode = 'KOR'
-	Group by District
-    order by SUM(Population) DESC;
+# 국내 광역시도의 인구수 합계를 내림차순으로 정렬
+SELECT District, SUM(Population) FROM city
+	WHERE CountryCode = 'KOR'
+	GROUP BY District
+	ORDER BY SUM(Population) DESC;
 
-# 국가별 도시의 갯수가 많은 나라 Top10
-Select Countrycode, COUNT(*) from city
-	Group by Countrycode
-	Order by COUNT(*) DESC
-	Limit 10;
+# 도시의 갯수가 많은 나라 Top 10
+SELECT CountryCode, COUNT(*) FROM city
+	GROUP BY CountryCode
+	ORDER BY COUNT(*) DESC
+	LIMIT 10;
+
+
+/*  1.6 그룹의 조건  */
 
 # 국내 도시의 갯수가 5개 이상인 도의 인구수 평균
 Select District, ROUND(AVG(Population)), COUNT(*) from city
@@ -130,7 +172,9 @@ SELECT Continent, SUM(SurfaceArea), ROUND(AVG(GNP)), COUNT(Name) From country
 	GROUP BY Continent
 	ORDER BY SUM(SurfaceArea) DESC;
 
-/* Join */
+
+/* 1.7 Join */
+
 # 도시의 갯수가 많은 나라 Top 10
 SELECT country.Name, city.CountryCode, COUNT(city.Name) FROM city
 JOIN country
@@ -167,3 +211,19 @@ JOIN countrylanguage AS r
 ON l.Code = r.CountryCode
 Where l.Continent = 'Asia'
 AND r.IsOfficial = TRUE;
+
+# 전 세계에서 인구가 가장 많은 10개 도시에서 사용하는 공식언어는? (도시명, 인구수, 언어명)
+SELECT l.Name AS country, l.Population, r.language FROM city AS l
+JOIN countrylanguage AS r
+ON l.CountryCode = r.CountryCode
+WHERE r.IsOfficial = true
+ORDER BY l.Population DESC
+LIMIT 10;
+
+
+/* 1.8 SubQuery */
+
+# 서울보다 인구가 많은 도시
+SELECT * FROM city
+	WHERE Population >
+	(SELECT Population FROM city WHERE Name = 'Seoul');
